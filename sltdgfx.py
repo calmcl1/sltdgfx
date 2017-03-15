e7a197c4a31f53a0f77c79507cb1c117b1453539
Cal McLean <calmcl1@aol.com>
2017-03-15 19:56:15 +0000

    Added basic judge UI

--- sltdgfx.py
+++ sltdgfx.py
@@ -1,6 +1,8 @@
 import wx
 #import wx.lib.agw.aui as aui
 import pycasper
+import sltd_scoreboard
+import sltd_couple
 
 import random
 random.seed()
@@ -45,108 +47,50 @@ class CoupleScoringJudgesPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
 
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer = wx.GridBagSizer(vgap=10, hgap=10)
 
-        num_templates = 5  # How many templates shall we have?
-        self.template_buttons = []
-        for i in xrange(0, num_templates):
-            btn = wx.Button(self,
-                            label="Recall Template {0}".format(i))
-            self.template_buttons.append(btn)
-            self.sizer.Add(btn)
+        self.couples_box = wx.ComboBox(
+            self, -1, style=wx.CB_READONLY, value="Couples")
+        self.couples_box.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.sizer.Add(self.couples_box, (0, 1), (1, 7), wx.EXPAND)
+
+        judge_score_font = wx.Font(48, wx.SWISS, wx.NORMAL, wx.BOLD)
+
+        self.judge_1_label = wx.StaticText(self, -1, "Judge 1")
+        self.sizer.Add(self.judge_1_label, (1, 1), (1, 1), wx.EXPAND)
+        self.judge_1_score = wx.TextCtrl(self, -1, style=wx.TE_CENTRE)
+        self.judge_1_score.SetFont(judge_score_font)
+        self.sizer.Add(self.judge_1_score, (2, 1), (1, 1), wx.EXPAND)
+
+        self.judge_2_label = wx.StaticText(self, -1, "Judge 2")
+        self.sizer.Add(self.judge_2_label, (1, 3), (1, 1), wx.EXPAND)
+        self.judge_2_score = wx.TextCtrl(self, -1, style=wx.TE_CENTRE)
+        self.judge_2_score.SetFont(judge_score_font)
+        self.sizer.Add(self.judge_2_score, (2, 3), (1, 1), wx.EXPAND)
+
+        self.judge_3_label = wx.StaticText(self, -1, "Judge 3")
+        self.sizer.Add(self.judge_3_label, (1, 5), (1, 1), wx.EXPAND)
+        self.judge_3_score = wx.TextCtrl(self, -1, style=wx.TE_CENTRE)
+        self.judge_3_score.SetFont(judge_score_font)
+        self.sizer.Add(self.judge_3_score, (2, 5), (1, 1), wx.EXPAND)
+
+        self.judge_4_label = wx.StaticText(self, -1, "Judge 4")
+        self.sizer.Add(self.judge_4_label, (1, 7), (1, 1), wx.EXPAND)
+        self.judge_4_score = wx.TextCtrl(self, -1, style=wx.TE_CENTRE)
+        self.judge_4_score.SetFont(judge_score_font)
+        self.sizer.Add(self.judge_4_score, (2, 7), (1, 1), wx.EXPAND)
+
+        self.calculate_score_btn = wx.Button(self, -1, "Calculate Score")
+        self.sizer.Add(self.calculate_score_btn, (4, 1), (1, 4), wx.EXPAND)
+
+        self.total_label = wx.StaticText(self, -1, "45", style=wx.TE_CENTRE)
+        self.total_label.SetFont(wx.Font(26, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.total_label.SetBackgroundColour(wx.LIGHT_GREY)
+        self.sizer.Add(self.total_label, (4, 6), (1, 2))
 
         self.SetAutoLayout(True)
         self.SetSizerAndFit(self.sizer)
         self.Layout()
-
-
-class Couple(object):
-
-    def __init__(self):
-
-        self.couple_name = ""
-        self.scores = [0, 0, 0, 0]
-        self.total = sum(self.scores)
-
-    def updateTotal(self):
-        self.total = sum(self.scores)
-
-
-class ScoreItem(wx.Panel):
-
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
-
-        self.sizer = wx.GridBagSizer(hgap=10)
-
-        self.position_no = wx.StaticText(self, -1, "")
-        self.position_no.SetFont(wx.Font(24,wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.position_no.SetMinSize((50,-1))
-        self.sizer.Add(self.position_no, (0, 0), (2, 1), wx.EXPAND)
-        self.couple_name = wx.StaticText(self, -1, "")
-        self.couple_name.SetFont(wx.Font(18,wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.sizer.Add(self.couple_name, (0, 1), (1, 4), wx.EXPAND)
-        self.score = wx.StaticText(self, -1, "", style=wx.ALIGN_RIGHT)
-        self.score.SetFont(wx.Font(24,wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.sizer.Add(self.score, (0,5), (1,1), wx.EXPAND)
-
-        scoreFont = wx.Font(14,wx.SWISS, wx.NORMAL, wx.NORMAL)
-        self.score_1 = wx.StaticText(self, -1, "")
-        self.score_1.SetFont(scoreFont)
-        self.sizer.Add(self.score_1, (1, 1), (1,1), wx.EXPAND)
-        self.score_2 = wx.StaticText(self, -1, "")
-        self.score_2.SetFont(scoreFont)
-        self.sizer.Add(self.score_2, (1, 2), (1,1), wx.EXPAND)
-        self.score_3 = wx.StaticText(self, -1, "")
-        self.score_3.SetFont(scoreFont)
-        self.sizer.Add(self.score_3, (1, 3), (1,1), wx.EXPAND)
-        self.score_4 = wx.StaticText(self, -1, "")
-        self.score_4.SetFont(scoreFont)
-        self.sizer.Add(self.score_4, (1, 4), (1,1), wx.EXPAND)
-
-        self.SetSizerAndFit(self.sizer)
-
-    def set_couple_name(self, name):
-        self.couple_name.SetLabel(name)
-
-    def set_scores(self, scores):
-        self.score_1.SetLabel("{0}".format(scores[0]))
-        self.score_2.SetLabel("{0}".format(scores[1]))
-        self.score_3.SetLabel("{0}".format(scores[2]))
-        self.score_4.SetLabel("{0}".format(scores[3]))
-
-    def set_total(self, total):
-        self.score.SetLabel("{0}".format(total))
-
-    def set_position(self, pos):
-        self.position_no.SetLabel("{0}".format(pos))
-
-
-class Scoreboard(wx.Panel):
-    """The right-hand-side scoreboard layout"""
-
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
-
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.scoreItems = []
-
-        self.SetAutoLayout(True)
-        self.SetSizer(self.sizer)
-        self.Layout()
-
-    def populateLeaderboard(self, coupleData):
-        i = 1
-        for couple in coupleData:
-            si = ScoreItem(self)
-            si.set_couple_name(couple.couple_name)
-            si.set_scores(couple.scores)
-            si.set_total(couple.total)
-            si.set_position(i)
-
-            self.sizer.Add(si, 1, wx.EXPAND)
-
-            i += 1
 
 
 class MainWindow(wx.Frame):
@@ -161,30 +105,32 @@ class MainWindow(wx.Frame):
 
         print "Loading UI..."
 
-        self.tab_window = wx.Notebook(self, -1)
+        self.tab_window = wx.Notebook(self, -1, style=wx.NB_TOP)
 
         self.panel_system_setup = SystemSetupPanel(self.tab_window)
         self.panel_scoring_judges = CoupleScoringJudgesPanel(self.tab_window)
 
-        self.tab_window.AddPage(self.panel_system_setup, "boxes", True)
-        self.tab_window.AddPage(self.panel_scoring_judges, "data", False)
+        self.tab_window.AddPage(self.panel_system_setup, "Scoring Setup", True)
+        self.tab_window.AddPage(self.panel_scoring_judges,
+                                "Couple Scoring: Judges", False)
 
-        self.sb = Scoreboard(self)
+        self.scoreboard = sltd_scoreboard.Scoreboard(self)
 
         couples = []
         for i in xrange(0, 10):
-            c = Couple()
+            c = sltd_couple.Couple()
             c.couple_name = "Couple {0}".format(i)
-            c.scores = [random.randint(0, 10), random.randint(
-                0, 10), random.randint(0, 10), random.randint(0, 10)]
-            c.updateTotal()
+            for i in xrange(0, 4):
+                c.set_score(i, random.randint(1, 10))
             couples.append(c)
 
-        self.sb.populateLeaderboard(couples)
+        couples = sorted(
+            couples, key=lambda couple: couple.total, reverse=True)
+        self.scoreboard.populateLeaderboard(couples)
 
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.sizer.Add(self.tab_window, 3, wx.EXPAND)
-        self.sizer.Add(self.sb, 1, wx.EXPAND)
+        self.sizer.Add(self.scoreboard, 1, wx.EXPAND)
 
         self.SetAutoLayout(True)
         self.SetSizer(self.sizer)
